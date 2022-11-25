206c314d2cdcd6b90d337bef74177e98fc0eb57a
Iskak <isnu260102@gmail.com>
2022-11-25 18:18:25 +0600

    init

---
+++ app/src/main/java/me/iskak/exchanger/LoadActivity.kt
@@ -0,0 +1,11 @@
+package me.iskak.exchanger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
